5a6fd6b28c3cecf43cdfea893e4382a5f8033831
Mikhil Shetty <111747532+Mikhil11@users.noreply.github.com>
2023-05-03 11:25:13 +0530

    Update runRapidTester.m

--- runRapidTester.m
+++ runRapidTester.m
@@ -1,24 +1,22 @@
 function runRapidTester(MATLABVersion)
 
-% load mat file if present
+% check if MAT file is available. If not available it means you are running it for the first MATLAB release in the matrix
 if isfile('mystructfilenew.mat')
     load('mystructfilenew.mat')
-    disp("found file")
 else
+    % The MATLAB version is added as fieldname to below struct variable and the results object is stored as its value
     resultComparisonReport = struct();
-    disp("created new struct")
 end
 
-% create testresults folder if needed
+% create testresults folder if tests are run for the first MATLAB release in the matrix
 if ~isfolder('testresults')
     mkdir('testresults')
-    disp("test folder created")
 end
 
-% create result folder inside testresults directory based on MATLAB version
+% create subfolder with MATLAB version as its name inside testresults folder
 resultFolder = fullfile("testresults",filesep,MATLABVersion);
 
-% run the rapid tester
+% run the rapid tester for a given MATLAB release
 folderPath = fullfile('cdt',filesep,'doc');
 obj = rapidTester(folderPath);
 obj.OutputPath = resultFolder;
@@ -26,7 +24,7 @@ obj.executeTests();
 results = obj.TestResults;
 
 
-%save failed results struct for diff report
+%save failed results inside the struct variable 'resultComparisonReport'
 failedResult = [results.Failed];
 resultComparisonReport.(MATLABVersion) = results(failedResult~=0);
 save('mystructfilenew.mat','resultComparisonReport');
